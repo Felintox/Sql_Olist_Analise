9419977f4da68961b8a2529895da73dda627969e
Gabriel Felinto <129033082+Felintox@users.noreply.github.com>
2025-08-11 20:36:00 -0300

    Update questao1.sql

--- questao1.sql
+++ questao1.sql
@@ -1,8 +1,21 @@
-SELECT product_category_name,count(product_category_name) as qnt_vendas
+-- Baseado na quantidade de produtos vendidos
+SELECT product_category_name as Categoria,count(product_category_name) as qtd_pedidos
 FROM olist_order_items  ol_item
-INNER JOIN olist_products ol_prod ON
+LEFT JOIN olist_products ol_prod ON
 ol_item.product_id=ol_prod.product_id
 GROUP BY product_category_name
-ORDER BY qnt_vendas DESC
+ORDER BY qtd_pedidos DESC
 LIMIT 10;
+
+-- Baseado no Ticket Médio de cada Categoria
+SELECT product_category_name as Categoria,SUM(ol_item.price) as preco_pedidos
+FROM olist_order_items  ol_item
+LEFT JOIN olist_products ol_prod ON
+ol_item.product_id=ol_prod.product_id
+WHERE
+  ol_prod.product_category_name IS NOT NULL AND ol_prod.product_category_name <> ''
+GROUP BY product_category_name
+ORDER BY preco_pedidos DESC
+LIMIT 10;
+
 
