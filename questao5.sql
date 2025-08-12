761c8afe862d8ffcb4e03f2f3c7e819ca655350d
Gabriel Felinto <129033082+Felintox@users.noreply.github.com>
2025-08-11 23:36:13 -0300

    Update questao5.sql

--- questao5.sql
+++ questao5.sql
@@ -1,9 +1,23 @@
-SELECT product_category_name, count(product_category_name) as pedido_cancelados
-FROM olist_orders oo
-JOIN 
-	olist_order_items oi on oo.order_id = oi.order_id
-JOIN 
-	olist_products p on oi.product_id = p.product_id
-where order_status='canceled'
-group by product_category_name
-order by pedido_cancelados DESC
+WITH CancelamentoPorVendedor AS (
+    SELECT
+        oi.seller_id,
+        COUNT(*) AS total_produtos,
+        COUNT(CASE WHEN o.order_status = 'canceled' THEN 1 END) AS total_cancelados
+    FROM
+        olist_order_items AS oi
+    INNER JOIN
+        olist_orders AS o ON oi.order_id = o.order_id
+    GROUP BY
+        oi.seller_id
+)
+SELECT
+    seller_id,
+    total_produtos,
+    total_cancelados,
+    ROUND((total_cancelados * 100.0 / total_produtos), 2) AS pct_cancelados
+FROM
+    CancelamentoPorVendedor
+WHERE
+    total_produtos > 10
+ORDER BY
+    pct_cancelados DESC;
