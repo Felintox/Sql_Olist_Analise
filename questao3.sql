72a90cd7651b3a7c3f5c3c7e5bb26d3afca9b751
Gabriel Felinto <129033082+Felintox@users.noreply.github.com>
2025-08-11 22:50:22 -0300

    Update questao3.sql

--- questao3.sql
+++ questao3.sql
@@ -13,3 +13,34 @@ FROM (
     where order_status='delivered'
     ) AS subconsulta
 GROUP BY status_entrega;
+
+
+WITH EntregasStatusPorEstado AS (
+    SELECT
+        c.customer_state,
+        CASE
+            WHEN o.order_delivered_customer_date <= o.order_estimated_delivery_date THEN 'No Prazo'
+            ELSE 'Atrasado'
+        END AS status_entrega
+    FROM
+        olist_orders AS o
+    INNER JOIN
+        olist_customers AS c ON o.customer_id = c.customer_id
+    WHERE
+        o.order_status = 'delivered'
+        AND o.order_delivered_customer_date IS NOT NULL
+        AND o.order_estimated_delivery_date IS NOT NULL
+)
+SELECT
+    customer_state AS Estado,
+    COUNT(CASE WHEN status_entrega = 'No Prazo' THEN 1 END) AS Entregas_No_Prazo,
+    COUNT(CASE WHEN status_entrega = 'Atrasado' THEN 1 END) AS Entregas_Atrasadas,
+    COUNT(*) AS Total_Entregas,
+    ROUND((COUNT(CASE WHEN status_entrega = 'No Prazo' THEN 1 END) * 100.0 / COUNT(*)), 2) AS Pct_No_Prazo,
+    ROUND((COUNT(CASE WHEN status_entrega = 'Atrasado' THEN 1 END) * 100.0 / COUNT(*)), 2) AS Pct_Atrasadas
+FROM
+    EntregasStatusPorEstado
+GROUP BY
+    customer_state
+ORDER BY
+    ROUND((COUNT(CASE WHEN status_entrega = 'Atrasado' THEN 1 END) * 100.0 / COUNT(*)), 2) DESC ;
