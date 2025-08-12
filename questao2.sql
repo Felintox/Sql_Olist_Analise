b33e4ccad0e263116fb83f33fbd298f4a85611a9
Gabriel Felinto <129033082+Felintox@users.noreply.github.com>
2025-08-11 23:09:55 -0300

    Update questao2.sql

--- questao2.sql
+++ questao2.sql
@@ -1,4 +1,21 @@
-select seller_id,count(product_id) as qnt_vendas
-from olist_order_items
-group by seller_id
-order by qnt_vendas DESC
+SELECT
+    t1.seller_id,
+    t3.seller_state AS estado_vendedor,
+    SUM(t1.price) AS faturamento_total,
+    COUNT(DISTINCT t1.order_id) AS qnt_pedidos,
+    COUNT(t1.product_id) AS qnt_produtos_vendidos,
+    SUM(t1.price) / COUNT(DISTINCT t1.order_id) AS ticket_medio
+FROM
+    olist_order_items AS t1
+LEFT JOIN
+    olist_orders AS t2 ON t1.order_id = t2.order_id
+LEFT JOIN
+    olist_sellers AS t3 ON t1.seller_id = t3.seller_id
+WHERE
+    YEAR(t2.order_purchase_timestamp) = 2017
+GROUP BY
+    t1.seller_id,
+    t3.seller_state
+ORDER BY
+    faturamento_total DESC
+LIMIT 20;
